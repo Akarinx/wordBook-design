3e7af07e90eb19576623e8b91346fd9ddd0b0377
Akarinx <690399014@qq.com>
2020-12-15 16:31:53 +0800

    fix:todolist样式修改

--- src/renderer/components/Todolist/Todolist.tsx
+++ src/renderer/components/Todolist/Todolist.tsx
@@ -1,9 +1,8 @@
-import { Collapse, Icon, Input, message, Steps } from 'antd';
+import { Collapse, Icon, Input, message, Steps, Tooltip } from 'antd';
 import React, { useCallback, useContext, useRef } from 'react';
 import { context, IContext } from '@/store/reducer'
 import { useState } from 'react';
 import s from './Todolist.module.scss'
-import Item from 'antd/lib/list/Item';
 export interface TodolistProps {
 
 }
@@ -17,8 +16,8 @@ const { Panel } = Collapse;
 const { Step } = Steps
 export const Todolist: React.FC<TodolistProps> = () => {
   const { state, dispatch } = useContext<IContext>(context)
-  const { todos } = state
   const input = useRef<Input>(null)
+  const { todos } = state
   const customPanelStyle = {
     background: '#fff1b8', // gold-2
     borderRadius: 4,
@@ -43,30 +42,42 @@ export const Todolist: React.FC<TodolistProps> = () => {
 
   return (<>
     <div className={s.TodolistWrapper}>
-      <Input ref={input} placeholder="请输入待办" onPressEnter={() => {
-        if (input.current) {
-          dispatch({
-            type: 'ADD',
-            payload: { todoName: input.current.input.value, todoType: 'todo' }
-          })
-          input.current.input.value = ''
-        }
-      }} />
+      <div className={s.TodolistInput}>
+        <Input
+          allowClear
+          ref={input}
+          placeholder="请输入待办"
+          suffix={
+            <Tooltip title="计划通">
+              <Icon type="info-circle" style={{ color: 'rgba(0,0,0,.45)' }} />
+            </Tooltip>
+          }
+          onPressEnter={() => {
+            if (input.current) {
+              dispatch({
+                type: 'ADD',
+                payload: { todoName: input.current.input.value, todoType: 'todo' }
+              })
+              input.current.setValue('')
+            }
+          }} />
+      </div>
+
       <Collapse
         bordered={false}
         expandIcon={({ isActive }) => <Icon type="caret-right" rotate={isActive ? 90 : 0} />}
       >
         {
           todos.map(item =>
-            (
-              <Panel header={<TodoHeader todoHeader={item.todoName} todotype={item.todoType} />} key={item.key} style={customPanelStyle}>
-                <Steps size="small" current={type2current[item.todoType]}>
-                  <Step title="Todo" icon={<Icon type="user" onClick={() => changeType(item.key, 'todo')} />} />
-                  <Step title="Pendding" icon={<Icon type="fire" onClick={() => changeType(item.key, 'pending')} />} />
-                  <Step title="Done" icon={<Icon type="smile-o" onClick={() => changeType(item.key, 'finished')} />} />
-                </Steps>
-              </Panel>
-            ))
+          (
+            <Panel header={<TodoHeader todoHeader={item.todoName} todotype={item.todoType} />} key={item.key} style={customPanelStyle}>
+              <Steps size="small" current={type2current[item.todoType]}>
+                <Step title="Todo" icon={<Icon type="user" onClick={() => changeType(item.key, 'todo')} />} />
+                <Step title="Pendding" icon={<Icon type="fire" onClick={() => changeType(item.key, 'pending')} />} />
+                <Step title="Done" icon={<Icon type="smile-o" onClick={() => changeType(item.key, 'finished')} />} />
+              </Steps>
+            </Panel>
+          ))
         }
 
       </Collapse>
@@ -82,14 +93,14 @@ const TodoHeader: React.FC<TodoHeaderProps> = (props) => {
       <span className={s.todoHeaderIcon}>
         {
           todotype === 'todo' && (
-            <Icon type="line" />
+            <Icon type="line" style={{ color: "blue" }} />
           ) ||
           todotype === 'pending' && (
-            <Icon type="loading" />
+            <Icon type="loading" style={{ color: "red" }} />
           ) ||
           todotype === 'finished' &&
           (
-            <Icon type="check" />
+            <Icon type="check" style={{ color: "green" }} />
           )
         }
       </span>
